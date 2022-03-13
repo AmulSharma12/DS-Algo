847b433c6319857c57b733fa05e943ff1e4051be
Amul Op <amulsharmarr67@gmail.com>
2022-03-13 23:33:50 +0530

    Unbounded Knapsack Problem

---
+++ Dp/Dp23_UnboundedKnapsack.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+
+public class Dp23_UnboundedKnapsack
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] wt = new int[n];
+		int[] val = new int[n];
+	}
+}
