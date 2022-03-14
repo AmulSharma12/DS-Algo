d601ae2dc42d83bbbb6f26b7038931f71d58c063
Amul Op <amulsharmarr67@gmail.com>
2022-03-14 20:12:36 +0530

    lcs-tabulation-spaceOptimize

--- Dp/Dp25_LCS.java
+++ Dp/Dp25_LCS.java
@@ -16,16 +16,18 @@ public class Dp25_LCS
 		int n = s2.length();
 		int[][] dp = new int[m+1][n+1];
 
-		for(int i = 0; i<=m; i++)
-		{
-			for(int j = 0; j<= n; j++)
-			{
-				dp[i][j] = -1;
-			}
-		}
+		// for(int i = 0; i<=m; i++)
+		// {
+		// 	for(int j = 0; j<= n; j++)
+		// 	{
+		// 		dp[i][j] = -1;
+		// 	}
+		// }
 
 		//gives the length of the longest common subsequence
-		System.out.println(lcs(m, n, s1, s2, dp));
+		// System.out.println(lcs(m, n, s1, s2, dp));
+		System.out.println(tabulation(m, n, s1, s2, dp));
+		System.out.println(spaceOptimization2D(m, n, s1, s2));
 	}
 
 
@@ -42,5 +44,76 @@ public class Dp25_LCS
 		if(x.charAt(i-1) == y.charAt(j-1))
 			return dp[i][j] = 1 + lcs(i-1, j-1, x, y, dp);
 		return dp[i][j] = Math.max(lcs(i, j-1, x, y, dp) , lcs(i-1, j, x, y, dp));
+	}
+
+
+
+
+	// tabulation -> solution
+	public static int tabulation(int m, int n, String x, String y, int[][] dp)
+	{
+
+		//base cases
+		for(int j = 0; j<=n; j++)
+			dp[0][j] = 0;
+
+		for(int i = 0; i<=m; i++)
+			dp[i][0] = 0;
+
+
+		//exploring all the index
+		for(int i = 1; i<=m; i++)
+		{
+			for(int j = 1; j<=n; j++)
+			{
+				if(x.charAt(i-1) == y.charAt(j-1))
+					dp[i][j] = 1 + dp[i-1][j-1];
+
+				else
+					dp[i][j] = Math.max(dp[i-1][j] , dp[i][j-1]);
+			}
+		}
+
+
+
+		//return the result
+		return dp[m][n];
+
+
+	}
+
+
+
+	// space optimisation
+	public static int spaceOptimization2D(int m, int n, String x, String y)
+	{
+		int[] prev = new int[n+1];
+		int[] curr = new int[n+1];
+
+		// base case
+		for(int j = 0; j<=n; j++)
+			prev[j] = 0;
+
+		
+		//exploring all the index
+		for(int i = 1; i<=m; i++)
+		{
+			for(int j = 1; j<=n; j++)
+			{
+				if(x.charAt(i-1) == y.charAt(j-1))
+					curr[j] = 1 + prev[j-1];
+
+				else
+					curr[j] = 0 + Math.max(prev[j] , curr[j-1]);
+			}
+
+			prev = curr;
+		}
+
+
+
+		//return the result
+		return prev[n];  
+
 	}
 }
