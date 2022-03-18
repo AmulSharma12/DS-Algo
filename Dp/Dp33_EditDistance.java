d80db21c4a9f26d2c82a4ddc43d8ceec79372d9e
Amul Op <amulsharmarr67@gmail.com>
2022-03-18 23:41:01 +0530

    1 based indexing done

--- Dp/Dp33_EditDistance.java
+++ Dp/Dp33_EditDistance.java
@@ -13,34 +13,34 @@ public class Dp33_EditDistance
 	}
 
 
-	 public static  int minDistance(String word1, String word2) {
+	 public static int minDistance(String word1, String word2) {
        int n = word1.length();
        int m = word2.length();
-        int[][] dp = new int[n][m];
+        int[][] dp = new int[n+1][m+1];
         
         for(int[] element : dp)
         {
             Arrays.fill(element, -1);
         }
-        return minOperations(n-1, m-1, word1, word2, dp);
+        return minOperations(n, m, word1, word2, dp);
         
     }
     
     
-    public static  int minOperations(int i, int j, String x, String y, int[][] dp)
+    public static int minOperations(int i, int j, String x, String y, int[][] dp)
     {
         //if string 1 exhausted then remaining of s2 will be no of insertion have to perform in s1
-        if(i<0)
-            return j+1;
+        if(i == 0)
+            return j;
         
         //same but no of deletion in s1
-        if(j<0)
-            return i+1;
+        if(j == 0)
+            return i;
         
         if(dp[i][j] != -1)
             return dp[i][j];
         
-        if(x.charAt(i) == y.charAt(j))
+        if(x.charAt(i-1) == y.charAt(j-1))
             return dp[i][j] = 0 + minOperations(i-1, j-1, x, y, dp);
         
         //now in no match 3 operations can be performed
