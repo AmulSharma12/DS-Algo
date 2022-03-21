b780d1aafecfecbdc243f41fc635b77deb6ab5bd
Amul Op <amulsharmarr67@gmail.com>
2022-03-21 16:28:37 +0530

    LeetCode83-> RemoveDuplicateSortedArrays

---
+++ LinkedList/P3LC83_RemoveDuplicateSortedArrays.java
@@ -0,0 +1,37 @@
+// LeetCode 83
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class P3_LC83_RemoveDuplicateSortedArrays
+{
+	public static void main(String[] args)
+	{
+		// function calling 
+		deletedDuplicates(head);
+
+	}
+
+
+	// code solution LeetCode 83 function
+	public ListNode deleteDuplicates(ListNode head) {
+        
+        //for empty linkedlist and single node 
+        if(head == null || head.next == null)
+            return head;
+        
+        //we check only if there next of temp exists
+        ListNode temp = head;
+        
+        while(temp.next != null)
+        {
+            if(temp.val == temp.next.val)
+                temp.next = temp.next.next;
+            else
+                temp = temp.next;
+        }
+        
+        return head;
+    }
+}
