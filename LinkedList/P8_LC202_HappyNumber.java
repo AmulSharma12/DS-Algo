1084c769b7500232f117315438f012319b419ade
Amul Op <amulsharmarr67@gmail.com>
2022-03-24 14:47:47 +0530

    LC202 HappyNumber

--- LinkedList/P8_LC202_HappyNumber.java
+++ LinkedList/P8_LC202_HappyNumber.java
@@ -14,7 +14,40 @@ public class P8_LC202_HappyNumber
 
 
 	// LeetCode 202 solution
-	 public static  boolean isHappy(int n) {
+	public static  boolean isHappy(int n) {
         
+        //it will use cycle detection approach only as if we fall in loop then there is pattern value will repeat 
+        int slow = n;
+        int fast = n;
+        
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }while(slow != fast);
+        
+        if(slow == 1)
+            return true;
+        
+        
+        return false;
+        
+    }
+    
+    
+    
+    //function finding the sum of squares of its digits
+    public static int findSquare(int n)
+    {
+        int ans = 0;
+        while(n > 0)
+        {
+            int rem = n % 10;
+            ans += rem* rem;
+            n /= 10;
+            
+        }
+        
+        
+        return ans;
     }
 }
