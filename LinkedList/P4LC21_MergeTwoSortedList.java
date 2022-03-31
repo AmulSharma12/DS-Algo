62dcc9c9bf486f7350a114373d0b400d31b1595c
Amul Op <amulsharmarr67@gmail.com>
2022-03-31 23:27:33 +0530

    merge two sorted lists

--- LinkedList/P4LC21_MergeTwoSortedList.java
+++ LinkedList/P4LC21_MergeTwoSortedList.java
@@ -30,7 +30,31 @@ public class P4LC21_MergeTwoSortedList
         
     }
 
-
+     //Approach -2 using recursion
+    public static  ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return merge(list1, list2);
+    }
+    
+    
+    public static  ListNode merge(ListNode l1, ListNode l2)
+    {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        
+        if(l1.val < l2.val)
+        {
+            l1.next = merge(l1.next, l2);
+            return l1;
+        }
+        else
+        {
+            l2.next = merge(l1, l2.next);
+            return l2;
+        }
+        
+    }
     
     
 }
